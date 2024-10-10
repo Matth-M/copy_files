fde8e182f2839f3cd98f44cd2183ae2cf36ff7f8
Matth-M <matthieu.matsima@gmail.com>
2024-10-10 03:52:13 +0200

    Copy file contents.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
-extern crate copypasta;
-
+use clap::{ArgAction, Parser};
 use copypasta::{ClipboardContext, ClipboardProvider};
+use std::path::Path;
+#[derive(Parser)]
+#[command(name = "copy_content")]
+#[command(version = "0.1")]
+#[command(about = "Copy contents of files selected files.", long_about = None)]
+struct Cli {
+    #[arg(action=ArgAction::Append)]
+    files: Vec<String>,
+}
 
 fn main() {
     let mut ctx = ClipboardContext::new().unwrap();
 
-    let msg = "Hello, world!";
-    ctx.set_contents(msg.to_owned()).unwrap();
+    let cli = Cli::parse();
+    let files = cli.files;
 
-    // let content = ctx.get_contents().unwrap();
-    //
-    // println!("{}", content);
+    let mut paths: Vec<Box<Path>> = vec![];
+    let mut msg = String::new();
+
+    for file in files {
+        let path = Path::new(&file);
+        if path.exists() {
+            let content = std::fs::read(path).unwrap_or("".into());
+            let content = String::from_utf8(content).unwrap_or("".to_string());
+            msg.push_str(&content);
+            paths.push(path.into());
+        }
+    }
+
+    ctx.set_contents(msg.to_owned()).unwrap();
     loop {}
 }
